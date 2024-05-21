1c48925fbbd5918c64353dd3d8011045bcfa9693
anastasiadonova <165403006+anastasiadonova@users.noreply.github.com>
2024-05-22 00:48:40 +0800

    Добавление новой модуль
    +возможность отправлять кириллицу
    +адресат

--- mail_assistant.py
+++ mail_assistant.py
@@ -1,15 +1,20 @@
 import smtplib                                                       #импорт сетевого протокола SMTP
+from email.mime.text import MIMEText                                 #создание текстовых объектов
+
 
 def send_email(message):                                             #создаем инструкию отправки сообщения  
     sender="pythonmailassistant@mail.ru"                             #отправитель и пароль от почты отправителя
     password="bJDZxDu4LJ1E1gLACBTC"
+    addressee="pythonmailassistant@mail.ru"                          #получатель
 
     server = smtplib.SMTP("smtp.mail.ru", 587)                       #доменное имя smpt-сервера и его порт
     server.starttls()                                                #подключение к шифрованию TLS
 
     try:                                                             #обработка исключений
         server.login(sender, password)                               #логин и пароль отправителя
-        server.sendmail(sender, sender, f"Subject: Important message!\n{message}") #отправитель, получатель, заголовок (тема) и само сообщение
+        msg = MIMEText(message)                                      #создание объекта - сообщения
+        msg["Subject"] = "Фотоотчёт"
+        server.sendmail(sender, addressee, msg.as_string())          #отправитель, получатель, заголовок (тема) и само сообщение
         return "Сообщение отправлено"                                #возврат сообщения в случае успешной отправки
     except Exception as _ex:                                         #обработка ошибок и возврат сообщения об ошибке
         return f"{_ex}\nНеверный логин или пароль."
