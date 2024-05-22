c2ddde40e61aca5fac68ba7f798a893f4bdca717
anastasiadonova <165403006+anastasiadonova@users.noreply.github.com>
2024-05-22 12:49:09 +0800

    Обработка и отправка изображения

--- mail_assistant.py
+++ mail_assistant.py
@@ -1,6 +1,9 @@
 import smtplib                                                       #импорт сетевого протокола SMTP
-from email.mime.text import MIMEText                                 #создание текстовых объектов
-
+import os
+import mimetypes
+from email.mime.text import MIMEText                                 #подключение модуля email.mime и его подсклассов
+from email.mime.multipart import MIMEMultipart                       
+from email.mime.application import MIMEApplication
 
 def send_email(message):                                             #создаем инструкию отправки сообщения  
     sender="pythonmailassistant@mail.ru"                             #отправитель и пароль от почты отправителя
@@ -12,15 +15,23 @@ def send_email(message):                                             #созда
 
     try:                                                             #обработка исключений
         server.login(sender, password)                               #логин и пароль отправителя
-        msg = MIMEText(message)                                      #создание объекта - сообщения
-        msg["Subject"] = "Фотоотчёт"
-        server.sendmail(sender, addressee, msg.as_string())          #отправитель, получатель, заголовок (тема) и само сообщение
-        return "Сообщение отправлено"                                #возврат сообщения в случае успешной отправки
+        msg = MIMEMultipart()                                        #основной объект
+        msg["Subject"] = "ВАЖНОЕ СООБЩЕНИЕ ДЛЯ ВАС!"                 #заголовок(тема) письма
+        text = MIMEText(message)                                     #создание объекта - сообщения
+        msg.attach(text)                                             #прикрепление к основному объекту
+        
+        file = MIMEApplication(open('image.jpg', 'rb').read())       #обработка, открытие и чтение изображения по байтам
+        file.add_header('Content-Disposition', 'attachment', filename='image.jpg')
+        msg.attach(file)                                             #прикрепление к основному объекту
+
+        server.sendmail(sender, addressee, msg.as_string())          #отправка сообщений
+
+        return "Сообщение отправлено"                                #возврат сообщения в случае успешной отправки  
     except Exception as _ex:                                         #обработка ошибок и возврат сообщения об ошибке
         return f"{_ex}\nНеверный логин или пароль."
 
 
-def main():                                                          #функция отправки сообщения 
+def main():                                                           
     message = input("Введите сообщение: ")                           #ввод сообщения
     print(send_email(message=message))                               #передача введенного сообщения 
 
